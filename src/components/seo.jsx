980c24b493a302b3b371b7dd97a92480b15378f5
bart016 <bart.turczynski@gmail.com>
2021-08-17 10:30:06 +0200

    test
    test

---
+++ src/components/seo.jsx
@@ -0,0 +1,72 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
+
+function Seo({ description, lang, meta, title, author }) {
+
+  const metaDescription = description
+  const defaultTitle = "Chodzą słuchy"
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `author`,
+          content: `@B&P Coders`
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+Seo.defaultProps = {
+  lang: `pl`,
+  meta: [],
+  description: ``,
+}
+
+Seo.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+}
+
+export default Seo
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import * as styles from "../styles/Index.module.scss"
+import Seo from "../components/seo"
+
 
 import Layout from "../components/Layout"
 
@@ -7,6 +9,7 @@ export default function Index()
 {
     return(
         <Layout>
+            <Seo title="Home"/>
             Strona główna
         </Layout>
     )
